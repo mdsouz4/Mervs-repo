c32bc4ad45da1690ce6fc2c71d2b96c5b14ccf23
mdsouz4 <62325015+mdsouz4@users.noreply.github.com>
2020-03-23 13:21:17 +0530

    Add files via upload

---
+++ Expand.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Expand{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter your string (number digits and alphabet letters alternating):");
+    String s = scan.nextLine();
+    String str = "";
+
+  }
+  public static String expand(String s)
+  {
+    
+  }
+
+}
